2f8c8667b15e33b420b5dd8086179362ef0065b4
v1itor <49135561+v1itor@users.noreply.github.com>
2021-04-15 13:49:23 -0300

    Add files via upload

---
+++ js/init.js
@@ -0,0 +1,12 @@
+  // Your web app's Firebase configuration
+  var firebaseConfig = {
+    apiKey: "AIzaSyCnC4iJhmHJqO2yO-RZ_BP7xwOxnqflghU",
+    authDomain: "todo-list-login-vitor.firebaseapp.com",
+    databaseURL: "https://todo-list-login-vitor.firebaseio.com",
+    projectId: "todo-list-login-vitor",
+    storageBucket: "todo-list-login-vitor.appspot.com",
+    messagingSenderId: "433359344018",
+    appId: "1:433359344018:web:cb487373890ca34c05b14b"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
